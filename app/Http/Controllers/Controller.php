16ecad2c2fd15cb7dcb86e886b4bfe0a7d6d5ab9
aminnur <azymo86@gmail.com>
2020-02-12 01:13:15 +0700

    set default initiation Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,16 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+
+  public function index ()
+  {
+    // json
+    $out = array(
+      'success' => true,
+      'message' => 'Request Accepted.'
+    );
+
+    return response()->json($out, 200);
+  }
+
 }
