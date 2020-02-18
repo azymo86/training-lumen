bc62dc3ad6b9313f525960bdaad7a27b70a35ca2
aminnur <azymo86@gmail.com>
2020-02-18 14:47:30 +0700

    update inventory model

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -7,4 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class Inventory extends Model
 {
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        // 'name', 'email',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        // 'password',
+    ];
+
 }
